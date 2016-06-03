c32127484f4373c8b8d26b519a3c43d09c4d0bbb
Felipe6659 <felipe6659@gmail.com>
2016-06-03 13:52:39 +0000

    Add files via upload

---
+++ BLL/BLLFornecedores.cs
@@ -0,0 +1,108 @@
+﻿using DAL;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class BLLFornecedores
+    {
+        private DALConecxao conecxao;
+
+        public BLLFornecedores(DALConecxao cx)
+        {
+            this.conecxao = cx;
+        }
+
+        public void Incluir(ModeloFornecedores modelo)
+        {
+
+            if (modelo.For_Nome.Trim().Length == 0)
+            {
+                throw new Exception("O nome do fornecedor é obrigatório");
+            }
+            else
+            {
+                if (modelo.For_Descricao.Trim().Length == 0)
+                {
+                    throw new Exception("A descrição do fornecedor é obrigatório");
+                }
+                else
+                {
+                    if (modelo.For_Endereco.Trim().Length == 0)
+                    {
+                        throw new Exception("O Endereço do fornecedor é obrigatório");
+                    }
+                }
+            }
+            
+            
+            //modelo.For_Nome = modelo.For_Nome.ToUpper();
+
+            DALFornecedores DALobj = new DALFornecedores(conecxao);
+            DALobj.Incluir(modelo);
+        }
+
+        public void Alterar(ModeloFornecedores modelo)
+        {
+           
+
+            if (modelo.For_IDFornecedor <= 0)  //Caso o identificador do fornecedor for menor que zero
+            {
+                throw new Exception("O identificador do fornecedor é obrigatório");
+            }
+            else
+            {
+                if (modelo.For_Nome.Trim().Length == 0)
+                {
+                    throw new Exception("O nome do fornecedor é obrigatório");
+                }
+                else
+                {
+                    if (modelo.For_Descricao.Trim().Length == 0)
+                    {
+                        throw new Exception("A descrição do fornecedor é obrigatório");
+                    }
+                    else
+                    {
+                        if (modelo.For_Endereco.Trim().Length == 0)
+                        {
+                            throw new Exception("O Endereço do fornecedor é obrigatório");
+                        }
+                    }
+                }
+            }
+
+            //modelo.For_Nome = modelo.For_Nome.ToUpper();
+
+            DALFornecedores DALobj = new DALFornecedores(conecxao);
+            DALobj.Alterar(modelo);
+        }
+
+
+        public void Excluir(int identificador)
+        {
+            DALFornecedores DALobj = new DALFornecedores(conecxao);
+            DALobj.Excluir(identificador);
+        }
+
+
+        public DataTable Localizar(String valor)
+        {
+            DALFornecedores DALobj = new DALFornecedores(conecxao);
+            return DALobj.Localizar(valor);
+        }
+
+
+        public ModeloFornecedores CarregaModeloFornecedores(int identificador)
+        {
+            DALFornecedores DALobj = new DALFornecedores(conecxao);
+            return DALobj.CarregaModeloFornecedores(identificador);
+        }
+
+    }
+}
---
+++ BLL/BLLTelefoneFornecedor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+using DAL;
+using System.Data;
+
+namespace BLL
+{
+    public class BLLTelefoneFornecedor
+    {
+        private DALConecxao conecxao;
+
+        public BLLTelefoneFornecedor(DALConecxao cx)
+        {
+            this.conecxao = cx;
+        }
+
+        public void Incluir(ModeloTelefoneFornecedor modelo)
+        {
+
+            if (modelo.Tel_Telefone.Trim().Length == 0)
+            {
+                throw new Exception("O telefone do fornecedor é obrigatório");
+            }
+            else
+            {
+                if (modelo.Tel_IDFornecedor <= 0)
+                {
+                    throw new Exception("O identficador do fornecedor é obrigatório");
+                }
+            }
+
+
+            //modelo.For_Nome = modelo.For_Nome.ToUpper();
+
+            DALTelefoneFornecedor DALobj = new DALTelefoneFornecedor(conecxao);
+            DALobj.Incluir(modelo);
+        }
+
+        public void Alterar(ModeloTelefoneFornecedor modelo)
+        {
+
+
+            if (modelo.Tel_IDTelefoneFornecedor <= 0)  //Caso o identificador do fornecedor for menor que zero
+            {
+                throw new Exception("O identificador do telefone do fornecedor é obrigatório");
+            }
+            else
+            {
+                if (modelo.Tel_Telefone.Trim().Length == 0)
+                {
+                    throw new Exception("O telefone do fornecedor é obrigatório");
+                }
+                else
+                {
+                    if (modelo.Tel_IDFornecedor <= 0)
+                    {
+                        throw new Exception("O identficador do fornecedor é obrigatório");
+                    }
+                }
+            }
+
+            //modelo.For_Nome = modelo.For_Nome.ToUpper();
+
+            DALTelefoneFornecedor DALobj = new DALTelefoneFornecedor(conecxao);
+            DALobj.Alterar(modelo);
+        }
+
+
+        public void Excluir(int identificador)
+        {
+            DALTelefoneFornecedor DALobj = new DALTelefoneFornecedor(conecxao);
+            DALobj.Excluir(identificador);
+        }
+
+
+        public DataTable Localizar(String valor)
+        {
+            DALTelefoneFornecedor DALobj = new DALTelefoneFornecedor(conecxao);
+            return DALobj.Localizar(valor);
+        }
+
+
+        public ModeloTelefoneFornecedor CarregaModeloTelefoneFornecedor(int identificador)
+        {
+            DALTelefoneFornecedor DALobj = new DALTelefoneFornecedor(conecxao);
+            return DALobj.CarregaModeloTelefoneFornecedor(identificador);
+        }
+    }
+}
